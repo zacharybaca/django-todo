6935257eba55045356fd732b3240e54124027dd6
zacharybaca <bacazach@icloud.com>
2021-05-05 08:17:26 -0500

    implemented delete todo

--- todo/views.py
+++ todo/views.py
@@ -5,6 +5,7 @@ from django.db import IntegrityError
 from django.contrib.auth import login, logout, authenticate
 from .forms import TodoForm
 from .models import Todo
+from django.utils import timezone
 
 # Create your views here.
 
@@ -74,3 +75,16 @@ def viewtodo(request, todo_pk):
             return redirect('currenttodos')
         except ValueError:
             return render(request, 'currenttodo.html', {'todo': todo, 'form': form, 'error': 'Bad Info'})
+
+def completetodo(request, todo_pk):
+   todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+   if request.method == 'POST':
+       todo.datecompleted = timezone.now() 
+       todo.save()
+       return redirect('currenttodos')
+
+def deletetodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == 'POST':
+        todo.delete()
+        return redirect('currenttodos')
--- todowoo/urls.py
+++ todowoo/urls.py
@@ -31,4 +31,7 @@ urlpatterns = [
     path('create/', views.createtodo, name='createtodo'),
     path('current/', views.currenttodos, name='currenttodos'),
     path('todo/<int:todo_pk>', views.viewtodo, name='viewtodo'),
+    path('todo/<int:todo_pk>/complete', views.completetodo, name='completetodo'),
+    path('todo/<int:todo_pk>/delete', views.deletetodo, name='deletetodo'),
+]
 ]
